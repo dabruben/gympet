e5f0ab5abd97a16e97c7f7c6987c570ef8c21815
marco <marcosclcheca@gmail.com>
2024-05-13 17:02:50 +0200

    Main Screen work bitch

--- app/src/main/java/com/dabutu/gympet/MainActivity.kt
+++ app/src/main/java/com/dabutu/gympet/MainActivity.kt
@@ -2,13 +2,28 @@ package com.dabutu.gympet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    fun hey(){
-        println("si")
+
+        val buttonObjetivos = findViewById<Button>(R.id.buttonObjetivos)
+        val buttonHorario = findViewById<Button>(R.id.btnEjercicios)
+        val buttonNutricion = findViewById<Button>(R.id.buttonNutricion)
+
+        buttonObjetivos.setOnClickListener {
+            // Intent para abrir la actividad de Objetivos
+        }
+
+        buttonHorario.setOnClickListener {
+            // Intent para abrir la actividad de Horario Semanal
+        }
+
+        buttonNutricion.setOnClickListener {
+            // Intent para abrir la actividad de Nutrición
+        }
     }
 }
